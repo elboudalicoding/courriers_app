1c837653d1fc704d189fed735a494574491c022b
ossama <ossamaelboudali56@gmail.com>
2025-01-25 17:50:01 +0100

    first pushing happend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center text-blue-600">
+        Loan Calculator
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src//*.{js,jsx,ts,tsx}", // Chemins des fichiers source
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
