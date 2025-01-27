3febabbd7b664d58ea0bc1dcdab74be3b14b2487
ossama <ossamaelboudali56@gmail.com>
2025-01-27 12:20:27 +0100

    hhhhhhhhhhhhh

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,1 +1,20 @@
- 
+const mysql = require("mysql");
+
+// Configure the database connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "courriersapp",
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.message);
+    return;
+  }
+  console.log("Connected to the MySQL database!");
+});
+
+module.exports = db;
